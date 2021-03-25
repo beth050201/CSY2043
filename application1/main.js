a4a54e054c09eb121ecfbaef16c420155663b627
beth050201 <56397955+beth050201@users.noreply.github.com>
2021-03-25 14:46:52 +0000

    mobile view

--- application1/main.js
+++ application1/main.js
@@ -10,3 +10,38 @@ function check(form)/*function to check email & password*/
    alert("Incorrect Email or Password")/*displays error message*/
   }
 }
+
+/* navigation found by watching video - https://www.youtube.com/watch?v=gXkqy0b4M5g */
+
+/*defining navSlide fucntion is equal to an anonymous function*/
+const navSlide= ()=> {
+  // const used as variable will not be reasssigned
+    const burger = document.querySelector('.burger'); //gets the burger
+    const nav = document.querySelector('.nav-links'); // gets the nav-links
+    const navLinks=document.querySelectorAll('.nav-links li'); // gets all the nav-links and li
+    
+     /*Toggle Navigation when burger is clicked
+     add eventlistner so when the burger is clicked it will carry out function*/
+  burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active'); // when burger is clicked get the class nav-active
+
+     /*burger animation*/
+      burger.classList.toggle('toggle'); // when burger is clicked get the toggle class 
+
+    /* adding animation to the links in the navigation - for each link run a function- mkaing delay
+   between each*/
+      navLinks.forEach((link,index) =>{ 
+      if(link.style.animation) {
+        link.style.animation='';
+       } else {
+       link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 1}s`; // altering time delay 
+      }
+     /*shows the delay for each link*/
+     console.log(index / 7);
+    });
+  });
+
+}
+
+/* envoking function used above*/
+navSlide();
